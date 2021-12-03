c87bee2cf0ecd97b369f33e9d01afde1090bd203
Steven DuBois <sdubois@csh.rit.edu>
2021-12-03 13:43:07 -0600

    Update settings.amezmo.php

--- settings.amezmo.php
+++ settings.amezmo.php
@@ -12,8 +12,8 @@ if (isset($_ENV['APP_HOSTNAME'])) {
   ];
 
   // Config 
-  $config_directories['active'] = BACKDROP_ROOT . '/config/active';
-  $config_directories['staging'] = BACKDROP_ROOT . '/config/staging';
+  $config_directories['active'] = BACKDROP_ROOT . '/files/config/active';
+  $config_directories['staging'] = BACKDROP_ROOT . '/files/config/staging';
 
 
   // Configure private and temporary file paths.
