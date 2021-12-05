ce0efd036ffd0ee3c401fd2a5083a4396549fd2a
Steven DuBois <srd2725@gmail.com>
2021-12-05 10:34:54 -0600

    clean up amezmo settings file

--- settings.amezmo.php
+++ settings.amezmo.php
@@ -1,5 +1,8 @@
 <?php
-
+/**
+ * @file
+ * Amezmo specific settings
+ */
 if (isset($_ENV['APP_HOSTNAME'])) {
   $databases['default']['default'] = [
     'database' => $_ENV['DB_DATABASE'],
@@ -11,27 +14,21 @@ if (isset($_ENV['APP_HOSTNAME'])) {
     'username' => $_ENV['DB_USER']
   ];
 
-  // Config 
-  $config_directories['active'] = '/webroot/storage/config/active';
-  $config_directories['staging'] = '/webroot/storage/config/staging';
+  // Config directories
+  $config_directories['active'] = $_ENV['STORAGE_DIRECTORY'] . '/config/active';
+  $config_directories['staging'] = $_ENV['STORAGE_DIRECTORY'] . 'config/staging';
 
 
   // Configure private and temporary file paths.
   $settings['file_private_path'] = $_ENV['STORAGE_DIRECTORY'] . '/private';
 
-  // // Configure the default PhpStorage and Twig template cache directories.
-  // $settings['php_storage']['default']['directory'] = $settings['file_private_path'];
-  // $settings['php_storage']['twig']['directory'] = $settings['file_private_path'];
-
   $settings['file_chmod_directory'] = 2775;
   $settings['file_chmod_file'] = 0664;
 
-  // // Set the project-specific entropy value, used for generating one-time
-  // // keys and such.
-  // // TODO: FIND A WAY TO REPLACE THIS WITH APP_KEY
-  $drupal_hash_salt  = $_ENV['APP_KEY'];
+  // Set the project-specific entropy value, used for generating one-time keys and such.
+  $settings['hash_salt'] = $_ENV['APP_KEY'];
 
 
-  $conf['file_temporary_path'] = '/tmp';
+  $config['system.core']['file_temporary_path'] = '/tmp';
 
 }
