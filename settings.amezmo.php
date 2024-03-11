76922c5638b7ff187f97c02d01d990f165306e9a
Jen Lampton <jen@jenerationweb.com>
2024-03-11 14:44:56 -0700

    Switch Drupal settings to Backdrop configuration overrides. Add docs.

--- settings.amezmo.php
+++ settings.amezmo.php
@@ -2,6 +2,13 @@
 /**
  * @file
  * Amezmo specific settings
+ *
+ * @see .amezmo/after-pull for how symlinks to these locations are created.
+ */
+
+/**
+ * Database configuration.
+ * - Adds Amezmo instance-specific database configuration options.
  */
 if (isset($_ENV['APP_HOSTNAME'])) {
   $databases['default']['default'] = [
@@ -14,21 +21,45 @@ if (isset($_ENV['APP_HOSTNAME'])) {
     'username' => $_ENV['DB_USER']
   ];
 
-  // Config directories
+  /**
+   * Site configuration files location.
+   * - By default config lives in the Amezmo persistant file storage location.
+   */
   $config_directories['active'] = $_ENV['STORAGE_DIRECTORY'] . '/config/active';
   $config_directories['staging'] = $_ENV['STORAGE_DIRECTORY'] . '/config/staging';
 
+  /**
+   * Trusted host configuration (optional but highly recommended).
+   * - Adds Amezmo instance-specific domains. @todo
+   *
+   * For example, this will allow the site to only run from www.example.com:
+   * @code
+   * $settings['trusted_host_patterns'] = array(
+   *   '^www\.example\.com$',
+   * );
+   * @endcode
+   */
 
-  // Configure private and temporary file paths.
-  $settings['file_private_path'] = $_ENV['STORAGE_DIRECTORY'] . '/private';
-
-  $settings['file_chmod_directory'] = 2775;
-  $settings['file_chmod_file'] = 0664;
-
-  // Set the project-specific entropy value, used for generating one-time keys and such.
+  /**
+   * Salt for one-time login links and cancel links, form tokens, etc.
+   */
   $settings['hash_salt'] = $_ENV['APP_KEY'];
 
+  /**
+   * Configuration overrides.
+   */
 
+  // Temporary file path.
   $config['system.core']['file_temporary_path'] = '/tmp';
+  // Private file path.
+  $config['system.core']['file_private_path'] = $_ENV['STORAGE_DIRECTORY'] . '/private';
+  // Public file path
+  // Note, this only works because of the symlink created in .amezmo/after.pull
+  $config['system.core']['file_public_path'] = '/files';
+
+  // Set permissions on files. Defaiult value is 0775.
+  $config['system.core']['file_chmod_file'] = 0664;
+  // Set permissions on directories. Defaiult value is 0775.
+  $config['system.core']['file_chmod_directory'] = 2775;
 
 }
